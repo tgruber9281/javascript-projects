bf3632cd772dc537da75c35f50f6222b9e49f049
Trevor Gruber <gruber.trevor9281@gmail.com>
2023-12-17 02:55:10 -0600

    Experimented with comments.

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,6 +1,6 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   console.log("This does not print." + " Until I removed the comment.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
@@ -8,5 +8,11 @@
    to have
    multi-line
    comments. */
+   
+   //Forward slash for comments, not back slash.
 
    console.log("Comments make your code more readable by others.");
+/*  I can comment out a line of code like this:
+
+   console.log("This won't print.");
+*/
