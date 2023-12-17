0186aabbf26b410090fc46ad0cc04ba9b064104e
Trevor Gruber <gruber.trevor9281@gmail.com>
2023-12-17 03:10:27 -0600

    Added space with \t and new line with \n

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,6 @@
 console.log('Hello, JavaScript.');
 console.log(2001);
 console.log("What","do","commas","do?");
-console.log("Does", "adding",      "space", "matter?");
+console.log("Does", "adding",      "\tspace", "matter?");
 console.log('Launch' + 'Code');
-console.log("LaunchCode was founded in", 2013);
+console.log("LaunchCode was \nfounded in", 2013);
