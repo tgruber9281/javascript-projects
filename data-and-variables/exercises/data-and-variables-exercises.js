f6c4ac07cc584208ac4ca14031106bc4dd3ca4a5
Trevor Gruber <gruber.trevor9281@gmail.com>
2023-12-18 22:04:59 -0600

    Data and Variables exercise complete

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,27 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttleName = 'Determination';
+let shuttleSpeedMPH = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 384400;
+const MPK = 0.621;
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMPH);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof MPK);
+
+let distanceToMarsMiles = distanceToMarsKm * MPK;
+let hoursToMars = distanceToMarsMiles / shuttleSpeedMPH;
+let daysToMars = hoursToMars / 24;
+
+console.log(shuttleName, "will take", daysToMars, "days to reach Mars.");
+
+let distanceToMoonMiles = distanceToMoonKm * MPK;
+let hoursToMoon = distanceToMoonMiles / shuttleSpeedMPH;
+let daysToMoon = hoursToMoon / 24;
+
+console.log(shuttleName, "will take", daysToMoon, "days to reach the Moon.");
