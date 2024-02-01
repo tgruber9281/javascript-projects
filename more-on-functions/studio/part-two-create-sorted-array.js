82965642baedd17e28ad2e5bb1d058f1c461d8a7
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-02-01 09:00:02 -0600

    More on Functions recursion task complete and notated.

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -44,21 +44,23 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
-// console.log(sortNumbers(nums2));
+
 let sortedArr = [];
 let indexOfMinValue = 0;
 
 function sortRecursion(arr) {
+  //if let sortedArr = []; was here, it would be reset to an empty array before having something added to it
+  //you could leave let indexOfMinValue = 0; as it gets a new value very loop anyways, but I went ahead and moved it outside the function.
   if (arr.length === 1) {
-    indexOfMinValue = arr.indexOf(findMinValue(arr));
-    sortedArr.push(arr[indexOfMinValue]);
-    arr.splice(indexOfMinValue, 1);
+    indexOfMinValue = arr.indexOf(findMinValue(arr)); //same code as in the while loop
+    sortedArr.push(arr[indexOfMinValue]); //same code as in the while loop
+    arr.splice(indexOfMinValue, 1); //same code as in the while loop
     return sortedArr;
   } else {
-    indexOfMinValue = arr.indexOf(findMinValue(arr));
-    sortedArr.push(arr[indexOfMinValue]);
-    arr.splice(indexOfMinValue, 1);
-    return sortRecursion(arr);
+    indexOfMinValue = arr.indexOf(findMinValue(arr));  //same code as in the while loop
+    sortedArr.push(arr[indexOfMinValue]); //same code as in the while loop
+    arr.splice(indexOfMinValue, 1); //same code as in the while loop
+    return sortRecursion(arr); //the else in a recursive function has to return the argument to the new call of the function with something different. in the factorial example we used "return integer * factorial(integer-1);".  I tried putting the splice function inside as return sortRecursion(arr.splice(indexOfMinValue,1)), but that passed the spliced value to the next loop rather than the array minus one value.  return sortRecursion(arr.slice(indexOfMinValue, indexOfMinValue + 1)) might work but I haven't tried it yet.
   }
 }
 
