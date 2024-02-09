3ce7c3788141d70126008abe93243a8ad30c7ea8
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-02-08 20:17:33 -0600

    Unit Testing Studio complete

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,29 @@
 
 let launchcode = {
-  
+  organization: 'nonprofit',
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: (num) => {
+    if (num%2 === 0 && num%3 !== 0 && num%5 !== 0){
+        return "Launch!";
+    } else if (num%2 !== 0 && num%3 === 0 && num%5 !== 0) {
+        return "Code!"
+    } else if (num%2 !== 0 && num%3 !== 0 && num%5 === 0) {
+        return "Rocks!"
+    } else if (num%2 === 0 && num%3 === 0 && num%5 !== 0) {
+        return "LaunchCode!"
+    } else if (num%2 !== 0 && num%3 === 0 && num%5 === 0) {
+        return "Code Rocks!"
+    } else if (num%2 === 0 && num%3 !== 0 && num%5 === 0) {
+        return "Launch Rocks!"
+    } else if (num%2 === 0 && num%3 === 0 && num%5 === 0) {
+        return "LaunchCode Rocks!"
+    } else /*if (num%2 !== 0 && num%3 !== 0 && num%5 !== 0) */{
+        return "Rutabagas! That doesn't work."
+    }
+
+  }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,41 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test('organization should be "nonprofit"', () => {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test('programs offered', () => {
+    expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"]);
+    expect(launchcode.programsOffered.length).toBe(3);
+  })
+
+  it('launchOutput should return Launch for input of 4', () => {
+    expect(launchcode.launchOutput(4)).toBe("Launch!");
+  })
+
+  it('input 3', () => {
+    expect(launchcode.launchOutput(9)).toBe("Code!");
+  })
+
+  it('input 25', () => {
+    expect(launchcode.launchOutput(25)).toBe("Rocks!")
+  })
+
+  it('input 6', () => {
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!')
+  })
+
+  it('input 15', () => {
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!')
+  })
+  it('input 10', () => {
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks!')
+  })
+  it('input 30', () => {
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!')
+  })
+  it('input 31', () => {
+    expect(launchcode.launchOutput(31)).toBe("Rutabagas! That doesn't work.")
+  })
 });
