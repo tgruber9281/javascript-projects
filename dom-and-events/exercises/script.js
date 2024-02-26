fdc23a7bbf7b0da72d3cfcddd88d6c83a23bdd50
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-02-25 18:34:28 -0600

    dom and events exercise complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,15 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', ()=>{paragraph.innerHTML = "Houston, we have liftoff!"});
+    missionAbort.addEventListener('mouseover', ()=>{missionAbort.style.backgroundColor = 'red'})
+    missionAbort.addEventListener('mouseleave', ()=>{missionAbort.style.backgroundColor = ''})
+    missionAbort.addEventListener('click', ()=>{
+        let abortConfirm = window.confirm("Are you sure you want to abort the mission?");
+        if (abortConfirm === true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    })
 }
 
 window.addEventListener("load", init);
