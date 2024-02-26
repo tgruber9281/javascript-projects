23b178e0682ce20dfca015f9ada31cea047b7b16
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-02-25 18:56:50 -0600

    playing around with listeners and selectors

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,17 +2,25 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
+    const windowBrowserVariable = document.getElementsByTagName('body');
+    console.log(windowBrowserVariable);
 
     // Put your code for the exercises here.
     button.addEventListener('click', ()=>{paragraph.innerHTML = "Houston, we have liftoff!"});
-    missionAbort.addEventListener('mouseover', ()=>{missionAbort.style.backgroundColor = 'red'})
-    missionAbort.addEventListener('mouseleave', ()=>{missionAbort.style.backgroundColor = ''})
+
+    missionAbort.addEventListener('mouseover', ()=>{missionAbort.style.backgroundColor = 'red'});
+    
+    missionAbort.addEventListener('mouseleave', ()=>{missionAbort.style.backgroundColor = ''});
+    
     missionAbort.addEventListener('click', ()=>{
         let abortConfirm = window.confirm("Are you sure you want to abort the mission?");
         if (abortConfirm === true) {
             paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
         }
-    })
+    });
+    // windowBrowserVariable[0].onmouseleave = ()=>{
+    //     window.confirm("Are you sure you want to leave?");
+    // };
 }
 
 window.addEventListener("load", init);
